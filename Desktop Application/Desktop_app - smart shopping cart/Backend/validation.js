c5e3698db3f7b49e958a29b3f6eca532f38b6621
piriyaraj <piriyaraj1998@egmail.com>
2021-10-19 01:20:20 +0530

    init

---
+++ Desktop Application/Desktop_app - smart shopping cart/Backend/validation.js
@@ -0,0 +1,45 @@
+//VALIDATION
+const Joi = require('@hapi/joi');
+
+const regValidation = (data) =>{
+
+
+    //validation schema
+const validUserSchema = Joi.object({
+    name : Joi.string().min(6).required(),
+    email : Joi.string().min(6).required().email(),
+    password : Joi.string().min(6).required(),
+    address:  Joi.string().required(),
+    nic:  Joi.string().required(),
+    phoneno:  Joi.string().required(),
+});
+
+    //const result = validUserSchema.validate(req.body);
+    //res.send(result);
+    return validUserSchema.validate(data).error;
+
+    
+}
+
+const logValidation = (data) =>{
+
+    console.log('login validation form');
+    //validation schema
+const validUserSchema = Joi.object({
+    name : Joi.string().min(6),
+    email : Joi.string().min(6).required().email(),
+    password : Joi.string().min(6).required()
+});
+
+    //const result = validUserSchema.validate(req.body);
+    //res.send(result); 
+    return validUserSchema.validate(data).error;
+
+    
+}
+
+
+module.exports.regValidation = regValidation;
+module.exports.logValidation = logValidation;
+
+
