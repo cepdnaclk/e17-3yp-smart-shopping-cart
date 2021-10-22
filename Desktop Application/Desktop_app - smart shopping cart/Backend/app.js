2b343a92f91bbd43c9bd96fcae0b86e98ce0d894
piriyaraj <piriyaraj1998@egmail.com>
2021-10-22 14:36:15 +0530

    port 80

--- Desktop Application/Desktop_app - smart shopping cart/Backend/app.js
+++ Desktop Application/Desktop_app - smart shopping cart/Backend/app.js
@@ -68,6 +68,6 @@ app.get('/', (req, res) => {
 });
 
 //LISTEN
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 app.listen(port, () => console.log(`listening on ${port}`));
 
