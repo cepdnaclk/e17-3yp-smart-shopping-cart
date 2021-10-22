c6c33817721ab90c9e3571ab17e3aea2a98348ea
piriyaraj <piriyaraj1998@egmail.com>
2021-10-22 15:59:24 +0530

    change for local

--- Desktop Application/Desktop_app - smart shopping cart/src/constant.js
+++ Desktop Application/Desktop_app - smart shopping cart/src/constant.js
@@ -1,3 +1,3 @@
 const hostAddress = "http://ec2-34-227-143-80.compute-1.amazonaws.com:5000/";
 const localhostAddress = "http://192.168.8.126:5000/";
-module.exports.hostAddress=hostAddress;
+module.exports.hostAddress = localhostAddress;
