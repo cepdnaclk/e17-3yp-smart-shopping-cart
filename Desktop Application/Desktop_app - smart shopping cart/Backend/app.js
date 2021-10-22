49294f68586242d67a57d0fa06426b44bb470922
piriyaraj <piriyaraj1998@egmail.com>
2021-10-22 13:42:36 +0530

    change port for https

--- Desktop Application/Desktop_app - smart shopping cart/Backend/app.js
+++ Desktop Application/Desktop_app - smart shopping cart/Backend/app.js
@@ -71,6 +71,6 @@ app.get('/', (req,res)=>{
  
  
 //LISTEN
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 443;
 app.listen(port, ()=>console.log(`listening on ${port}`));
 
