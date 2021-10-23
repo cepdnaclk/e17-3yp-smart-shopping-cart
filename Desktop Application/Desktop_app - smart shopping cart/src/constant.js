7daa8edc26e5597fcbdc3a1f758efbea0cfd7cf0
piriyaraj <piriyaraj1998@egmail.com>
2021-10-23 10:04:22 +0530

    commented

--- Desktop Application/Desktop_app - smart shopping cart/src/constant.js
+++ Desktop Application/Desktop_app - smart shopping cart/src/constant.js
@@ -1,3 +1,5 @@
+// for run locally comment2nd line and uncomment 3 line and change with your local ip
+
 const hostAddress = "https://ssc.walinking.link/";
 // const hostAddress = "http://192.168.8.126:5000/";
 module.exports.hostAddress = hostAddress;
