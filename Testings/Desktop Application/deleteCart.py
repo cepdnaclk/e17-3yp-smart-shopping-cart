c3b2ba9bd6b1a16dbcbc6ca2f2f2edaf06781429
piriyaraj <piriyaraj1998@egmail.com>
2021-10-23 15:11:14 +0530

    added delete cart,items and item update tesings

--- Testings/Desktop Application/deleteCart.py
+++ Testings/Desktop Application/deleteCart.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.alert import Alert
 import time
 import pyperclip
 from selenium.webdriver.common.keys import Keys
@@ -13,14 +14,24 @@ import timeit
 import traceback
 import loginTest
 
-def deleteItem(driver):
-    time.sleep(5)
-    itemTab=driver.find_element_by_xpath("//*[@id='sidebar']/ul/li[2]/a/span[1]")
-    itemTab.click()
-    time.sleep(5)
-    deleteButton = driver.find_element_by_xpath("//*[@id='main-content']/section/div[2]/div/section/table/tbody/tr[2]/div[2]/a")
-    deleteButton.click()
 
+def deleteCart(driver):
+    time.sleep(5)
+    cartTab = driver.find_element_by_xpath("//*[@id='sidebar']/ul/li[3]/a")
+    cartTab.click()
+
+    time.sleep(5)
+    deleteButton = driver.find_element_by_xpath("//*[@id='main-content']/section/div[2]/div/section/table/tbody/tr[2]/td[6]/div/button/i")
+    deleteButton.click()
+    time.sleep(5)
+
+    time.sleep(5)
+    alert = Alert(driver)
+    alert.accept()
+
+    time.sleep(5)
+    alert = Alert(driver)
+    alert.accept()
 
     pass
 
@@ -30,10 +41,9 @@ if (__name__=="__main__"):
     # help to prevent close automatically
     options.add_experimental_option("detach", True)
 
-    driver = webdriver.Chrome(
-        options=options, executable_path='chromedriver.exe')  # give chrome driver path
+    driver = webdriver.Chrome(options=options, executable_path='chromedriver.exe')  # give chrome driver path
     driver.maximize_window()
     loginTest.login(driver)
-    deleteItem(driver)
+    deleteCart(driver)
 
     pass
