a73b4c4a6f4a7a2e81b41ecbac0a03d73cb8ead2
piriyaraj <piriyaraj1998@egmail.com>
2021-10-23 09:43:25 +0530

    added https backend url

--- Desktop Application/Desktop_app - smart shopping cart/src/constant.js
+++ Desktop Application/Desktop_app - smart shopping cart/src/constant.js
@@ -1,3 +1,3 @@
-const hostAddress = "http://ec2-34-227-143-80.compute-1.amazonaws.com:5000/";
+const hostAddress = "https://ssc.walinking.link/";
 // const hostAddress = "http://192.168.8.126:5000/";
 module.exports.hostAddress = hostAddress;
